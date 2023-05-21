b007bffbd1104526f5c8208baa46826ec7a3a1f3
Meynard <meynard.trinidad44@gmail.com>
2023-05-21 10:05:39 +0800

    Fixed factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,14 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
+        \App\Models\Role::factory()->create([
+            'name' => 'admin'
+        ]);
+
+        \App\Models\Role::factory()->create([
+            'name' => 'user'
+        ]);
+
         \App\Models\User::factory()->create([
             'first_name' => 'Admin',
             'last_name' => 'Admin',
@@ -36,72 +44,56 @@ class DatabaseSeeder extends Seeder
             'email' => 'user2@example.com',
         ]);
 
-        // DB::table('users')->insert([
-        //     [
-        //         'first_name' => 'Admin',
-        //         'last_name' => 'Admin',
-        //         'username' => 'admin',
-        //         'email' => 'admin@example.com',
-        //     ],
-        //     [
-        //         'first_name' => 'John',
-        //         'last_name' => 'Doe',
-        //         'username' => 'john_d',
-        //         'email' => 'user1@example.com',
-        //     ],
-        //     [
-        //         'first_name' => 'Jane',
-        //         'last_name' => 'Doe',
-        //         'username' => 'jane_d',
-        //         'email' => 'user2@example.com',
-        //     ]
-        // ]);
-
-        DB::table('roles')->insert([
-            [
-                'name' => 'admin'
-            ],
-            [
-                'name' => 'user'
-            ],
-        ]);
-
         DB::table('barangays')->insert([
             [
                 'name' => 'Bulilan Sur',
                 'municipality' => 'Laguna',
-                'province' => 'Pila'
+                'province' => 'Pila',
+                'created_at' => now(),
+                'updated_at' => now()
             ],
             [
                 'name' => 'Labuin',
                 'municipality' => 'Laguna',
-                'province' => 'Santa Cruz'
+                'province' => 'Santa Cruz',
+                'created_at' => now(),
+                'updated_at' => now()
             ],
         ]);
 
         DB::table('user_roles')->insert([
             [
                 'user_id' => 1,
-                'role_id' => 1
+                'role_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now()
             ],
             [
                 'user_id' => 2,
-                'role_id' => 2
+                'role_id' => 2,
+                'created_at' => now(),
+                'updated_at' => now()
             ],
             [
                 'user_id' => 3,
-                'role_id' => 2
+                'role_id' => 2,
+                'created_at' => now(),
+                'updated_at' => now()
             ],
         ]);
 
         DB::table('user_barangays')->insert([
             [
                 'user_id' => 2,
-                'barangay_id' => 1
+                'barangay_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now()
             ],
             [
                 'user_id' => 3,
-                'barangay_id' => 2
+                'barangay_id' => 2,
+                'created_at' => now(),
+                'updated_at' => now()
             ],
         ]);
     }
