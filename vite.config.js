00eea6f2d1650659e129adf999b6163667987fa9
Miguel Arciaga <miguelarciagaa@gmail.com>
2023-05-22 00:27:59 +0800

    composer vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/accounts.js', 'resources/js/bootstrap.js', 'resources/js/calendar.js', 'resources/js/datetime.js', 'resources/js/nav.js', 'resources/js/table.js'],
             refresh: true,
         }),
     ],
