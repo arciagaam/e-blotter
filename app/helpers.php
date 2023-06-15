98cdd4721ee90706cbdfab415441826d53d4fed2
Meynard <meynard.trinidad44@gmail.com>
2023-06-15 21:00:11 +0800

    Created helper function for alert message

--- app/helpers.php
+++ app/helpers.php
@@ -2,7 +2,8 @@
 
 use App\Models\LoginTrail;
 
-function addToLoginTrail($login_role_id) {
+function addToLoginTrail($login_role_id)
+{
     $loginTrail = New LoginTrail();
     $loginTrail->barangays()->associate(auth()->user()->barangays[0]->id);
     $loginTrail->users()->associate(auth()->user()->id);
@@ -21,4 +22,9 @@ function getKpRelations($kp_form_id)
     ];
 
     return $relations[$kp_form_id] ?? [];
+}
+
+function setAlertMessage(string $title, string $description, string $type = 'information') 
+{
+    session()->flash('alert', ['title' => $title, 'description' => $description, 'type' => $type]);
 }
