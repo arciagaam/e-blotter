2cbddcec95b2ac6317c748f046f5bd331302b2d3
polaski0 <meynard.trinidad44@gmail.com>
2023-07-25 07:08:52 +0800

    Created function for the message in GetKpFormMessageActions

--- app/Actions/GetKpFormMessageActions.php
+++ app/Actions/GetKpFormMessageActions.php
@@ -8,14 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class GetKpFormMessageActions
 {
+    public function generateMessage(string|null $message, string|null $recommendations, array $formIds): array
+    {
+        return [
+            'message' => $message,
+            'recommendations' => $recommendations,
+            'form_ids' => $formIds
+        ];
+    }
 
     public function getKpForm7Message(): array
     {
-        return [
-            'message' => 'Form 7 Issued',
-            'recommendations' => 'Issue Form 8 and 9',
-            'form_ids' => [8, 9]
-        ];
+        return $this->generateMessage('Form 7 Issued', 'Issue Form 8 and 9', [8, 9]);
     }
 
     public function getKpForm8and9Message(string $record_id, RecordKpFormActions $action): array
@@ -23,17 +27,9 @@ class GetKpFormMessageActions
         $issuedKpForms = $action->checkIssuedKpForms($record_id, [8, 9]);
 
         if ($issuedKpForms->contains('kp_form_id', 8) && !$issuedKpForms->contains('kp_form_id', 9)) {
-            return [
-                'message' => 'Form 8 Issued',
-                'recommendations' => 'Issue Form 9',
-                'form_ids' => [9]
-            ];
+            return $this->generateMessage('Form 8 Issued', 'Issue Form 9', [9]);
         } else if (!$issuedKpForms->contains('kp_form_id', 8) && $issuedKpForms->contains('kp_form_id', 9)) {
-            return [
-                'message' => 'Form 9 Issued',
-                'recommendations' => 'Issue Form 8',
-                'form_ids' => [8]
-            ];
+            return $this->generateMessage('Form 9 Issued', 'Issue Form 8', [8]);
         }
 
         $attempts = $action->checkAttempts($record_id, [8, 9]);
@@ -43,116 +39,56 @@ class GetKpFormMessageActions
         if (count($attempts)) {
             if ($hearingDates->hasAny([8, 9]) && ($now > strtotime($hearingDates[8]->value) || $now > strtotime($hearingDates[9]->value))) {
                 if ($attempts->contains('kp_form_id', 8) && !$attempts->contains('kp_form_id', 9)) {
-                    return [
-                        'message' => 'Maximum attempts reached for Complainant/s',
-                        'recommendations' => 'Issue Form 18',
-                        'form_ids' => [18]
-
-                    ];
+                    return $this->generateMessage('Maximum attempts reached for Complainant/s', 'Issue Form 18', [18]);
                 } else if (!$attempts->contains('kp_form_id', 8) && $attempts->contains('kp_form_id', 9)) {
-                    return [
-                        'message' => 'Maximum attempts reached for Respondent/s',
-                        'recommendations' => 'Issue Form 19',
-                        'form_ids' => [19]
-
-                    ];
+                    return $this->generateMessage('Maximum attempts reached for Respondent/s', 'Issue Form 19', [19]);
                 } else if ($attempts->contains('kp_form_id', 8) && $attempts->contains('kp_form_id', 9)) {
-                    return [
-                        'message' => 'Maximum attempts reached for both Complainant/s and Respondent/s',
-                        'recommendations' => 'Issue Form 18 and 19',
-                        'form_ids' => [18, 19]
-
-                    ];
+                    return $this->generateMessage('Maximum attempts reached for both Complainant/s and Respondent/s', 'Issue Form 18 and 19', [18, 19]);
                 }
             }
         }
 
         if (count($hearingDates)) {
             if ($hearingDates->has([8, 9]) && ($now > strtotime($hearingDates[8]->value) && $now > strtotime($hearingDates[9]->value))) {
-                return [
-                    'message' => "Form 8 and 9 are both past the hearing date",
-                    'recommendations' => "Issue Form 8 and 9",
-                    'form_ids' => [8, 9]
-
-                ];
+                return $this->generateMessage('Form 8 and 9 are both past the hearing date', 'Issue Form 8 and 9', [8, 9]);
             } else if ($hearingDates->has(8) && ($now > strtotime($hearingDates[8]->value))) {
-                return [
-                    'message' => "Form 8 is past the hearing date",
-                    'recommendations' => "Issue Form 8",
-                    'form_ids' => [8]
-
-                ];
+                return $this->generateMessage('Form 8 is past the hearing date', 'Issue Form 8', [8]);
             } else if ($hearingDates->has(9) && ($now > strtotime($hearingDates[9]->value))) {
-                return [
-                    'message' => "Form 9 is past the hearing date",
-                    'recommendations' => "Issue Form 9",
-                    'form_ids' => [9]
-                ];
+                return $this->generateMessage('Form 9 is past the hearing date', 'Issue Form 9', [9]);
             }
         }
 
-        return [
-            'message' => null,
-            'recommendations' => null,
-            'form_ids' => []
-        ];
+        return $this->generateMessage(null, null, []);
     }
 
     public function getKpForm10Message()
     {
-        return [
-            'message' => 'Form 10 Issued',
-            'recommendations' => 'Issue Form 11',
-            'form_ids' => [11]
-        ];
+        return $this->generateMessage('Form 10 Issued', 'Issue Form 11', [11]);
     }
 
     public function getKpForm11Message()
     {
-        return [
-            'message' => 'Form 11 Issued',
-            'recommendations' => 'Issue Form 12',
-            'form_ids' => [12]
-
-        ];
+        return $this->generateMessage('Form 11 Issued', 'Issue Form 12', [12]);
     }
 
     public function getKpForm12Message()
     {
-        return [
-            'message' => 'Form 12 Issued',
-            'recommendations' => 'Issue Form 13',
-            'form_ids' => [13]
-
-        ];
+        return $this->generateMessage('Form 12 Issued', 'Issue Form 13', [13]);
     }
 
     public function getKpForm13Message()
     {
-        return [
-            'message' => 'Form 13 Issued',
-            'recommendations' => 'Issue Form 14 if both parties attended the hearing, hence, issue forms 18 and 19',
-            'form_ids' => [14, 18, 19]
-        ];
+        return $this->generateMessage('Form 13 Issued', 'Issue Form 14 if both parties attended the hearing, hence, issue forms 18 and 19', [14, 18, 19]);
     }
 
     public function getKpForm14Message()
     {
-        return [
-            'message' => 'Form 14 Issued',
-            'recommendations' => 'Issue Form 15',
-            'form_ids' => [15]
-
-        ];
+        return $this->generateMessage('Form 14 Issued', 'Issue Form 15', [15]);
     }
 
     public function getKpForm15Message()
     {
-        return [
-            'message' => 'Form 15 Issued',
-            'recommendations' => 'Issue Form 16',
-            'form_ids' => [16]
-        ];
+        return $this->generateMessage('Form 15 Issued', 'Issue Form 16', [16]);
     }
 
     public function getKpForm16Message(string $record_id)
@@ -163,28 +99,15 @@ class GetKpFormMessageActions
         $difference = Carbon::parse($now)->diffInDays(Carbon::parse($record->kp_deadline->format('Y-m-d')), false);
 
         if ($difference > 0) {
-            return [
-                'message' => 'Form 16 Issued',
-                'recommendations' => "Issue Form 25 after $difference day/s",
-                'form_ids' => [16]
-
-            ];
+            return $this->generateMessage('Form 16 Issued', "Issue Form 25 after $difference day/s", []);
         }
 
-        return [
-            'message' => '10 days has passed since the issuance of Form 16',
-            'recommendations' => 'Issue Form 25',
-            'form_ids' => [25]
-        ];
+        return $this->generateMessage('10 days has passed since the issuance of Form 16', "Issue Form 25", [25]);
     }
 
     public function getKpForm17Message()
     {
-        return [
-            'message' => 'Form 17 Issued',
-            'recommendations' => 'Issue Form 20',
-            'form_ids' => [20]
-        ];
+        return $this->generateMessage('Form 17 Issued', 'Issue Form 20', [20]);
     }
 
     public function getKpForm18and19Message(string $record_id, RecordKpFormActions $action): array
@@ -194,32 +117,15 @@ class GetKpFormMessageActions
 
         if (count($hearingDates)) {
             if ($hearingDates->has([18, 19]) && ($now > strtotime($hearingDates[18]->value) && $now > strtotime($hearingDates[19]->value))) {
-                return [
-                    'message' => "Form 18 and 19 are both past the hearing date",
-                    'recommendations' => "Close the case.",
-                    'form_ids' => []
-                ];
+                return $this->generateMessage('Form 18 and 19 are both past the hearing date', 'Close the case', []);
             } else if ($hearingDates->has(18) && $now > strtotime($hearingDates[18]->value)) {
-                return [
-                    'message' => "Form 18 is past the hearing date",
-                    'recommendations' => "Close the case.",
-                    'form_ids' => []
-
-                ];
+                return $this->generateMessage('Form 18 is past the hearing date', 'Close the case', []);
             } else if ($hearingDates->has(19) && $now > strtotime($hearingDates[19]->value)) {
-                return [
-                    'message' => "Form 19 is past the hearing date",
-                    'recommendations' => "Issue Form 20",
-                    'form_ids' => [20]
-                ];
+                return $this->generateMessage('Form 19 is past the hearing date', 'Issue Form 20', [20]);
             }
         }
 
-        return [
-            'message' => null,
-            'recommendations' => null,
-            'form_ids' => []
-        ];
+        return $this->generateMessage(null, null, []);
     }
 
     public function getKpForm20Message(string $record, RecordKpFormActions $action) : array
@@ -227,71 +133,39 @@ class GetKpFormMessageActions
         $issuedKpForms = $action->checkIssuedKpForms($record, [16]);
 
         if(count($issuedKpForms)) {
-            return [
-                'message' => 'Form 20 Issued and agreement from Form 16 was not followed.',
-                'recommendations' => 'Issue Form 21',
-                'form_ids' => [21]
-            ];
+            return $this->generateMessage('Form 20 Issued and agreement from Form 16 was not followed', 'Issue Form 21', [21]);
         } else {
-            return [
-                'message' => 'Form 20 Issued and either parties did not attend the hearing.',
-                'recommendations' => 'Issue Form 22',
-                'form_ids' => [22]
-            ];
+            return $this->generateMessage('Form 20 Issued and either parties did not attend the hearing', 'Issue Form 22', [22]);
         }
     }
 
     public function getKpForm21and22Message(string $latestKpForm) : array
     {
         if($latestKpForm == 21) {
-            return [
-                'message' => 'Form 21 Issued',
-                'recommendations' => 'Issue Form 23',
-                'form_ids' => [23]
-            ];
+            return $this->generateMessage('Form 21 Issued', 'Issue Form 23', [23]);
         } else {
-            return [
-                'message' => 'Form 22 Issued',
-                'recommendations' => 'Issue Form 23',
-                'form_ids' => [23]
-            ];
+            return $this->generateMessage('Form 22 Issued', 'Issue Form 23', [23]);
         }
     }
 
     public function getKpForm23Message()
     {
-        return [
-            'message' => 'Form 23 Issued',
-            'recommendations' => 'Issue Form 24',
-            'form_ids' => [24]
-        ];
+        return $this->generateMessage('Form 23 Issued', 'Issue Form 24', [24]);
     }
 
     public function getKpForm24Message()
     {
-        return [
-            'message' => 'Form 24 Issued',
-            'recommendations' => 'Close Case',
-            'form_ids' => []
-        ];
+        return $this->generateMessage('Form 24 Issued', 'Close the case', []);
     }
 
     public function getKpForm25Message()
     {
-        return [
-            'message' => 'Form 25 Issued',
-            'recommendations' => 'Issue Form 27',
-            'form_ids' => [27]
-        ];
+        return $this->generateMessage('Form 25 Issued', 'Issue Form 27', [27]);
     }
 
     public function getKpForm27Message()
     {
-        return [
-            'message' => 'Form 27 Issued',
-            'recommendations' => 'Close Case',
-            'form_ids' => []
-        ];
+        return $this->generateMessage('Form 27 Issued', 'Close the case', []);
     }
 
 }
--- app/helpers.php
+++ app/helpers.php
@@ -1,24 +1,34 @@
 <?php
 
-use App\Models\LoginTrail;
+// use App\Models\LoginTrail;
 
-function addToLoginTrail($login_role_id)
-{
-    $loginTrail = New LoginTrail();
-    $loginTrail->barangays()->associate(auth()->user()->barangays[0]->id);
-    $loginTrail->users()->associate(auth()->user()->id);
-    $loginTrail->loginroles()->associate($login_role_id);
-    $loginTrail->save();
-}
+// function addToLoginTrail($login_role_id)
+// {
+//     $loginTrail = New LoginTrail();
+//     $loginTrail->barangays()->associate(auth()->user()->barangays[0]->id);
+//     $loginTrail->users()->associate(auth()->user()->id);
+//     $loginTrail->loginroles()->associate($login_role_id);
+//     $loginTrail->save();
+// }
 
 function getKpRelations($kp_form_id)
 {
     $relations = [
-        '8' => ['7', '9'],
-        '9' => ['7', '8'],
-        '18' => ['8'],
-        '19' => ['9'],
-        '25' => ['16']
+        // '8' => ['7', '9'],
+        // '9' => ['7', '8'],
+        // '18' => ['8'],
+        // '19' => ['9'],
+        // '25' => ['15', '16'],
+        8 => [7, 9],
+        9 => [7, 8],
+        15 => [11],
+        18 => [8],
+        19 => [9],
+        20 => [17, 18, 19],
+        21 => [20],
+        22 => [20],
+        23 => [16],
+        25 => [15, 16],
     ];
 
     return $relations[$kp_form_id] ?? [];
