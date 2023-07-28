e551701cfb79738550698008f678d36d2b887a20
polaski0 <meynard.trinidad44@gmail.com>
2023-07-28 21:06:16 +0800

    Added display on kp form

--- resources/views/pages/user/kp_form/show.blade.php
+++ resources/views/pages/user/kp_form/show.blade.php
@@ -1,5 +1,22 @@
 <x-layout>
-    <x-page-header>KP Forms</x-page-header>
+    <x-page-header>KP Form # {{$id}}</x-page-header>
 
-    @include("kp_forms.preview.$id")
+    <div class="flex lg:justify-center lg:items-center">
+        <div class="flex flex-col gap-4">
+            <div class="bg-white w-fit p-8 border border-slate-400 rounded-md min-w-fit">
+                @include("kp_forms.preview.$id")
+            </div>
+            <button class="btn-outline w-fit h-fit self-start lg:self-end" id="print">Print</button>
+        </div>
+    </div>
 </x-layout>
+
+<script>
+    window.addEventListener('load', () => {
+        const printBtn = document.querySelector('#print');
+        
+        printBtn.addEventListener('click', () => {
+            window.print();
+        });
+    });
+</script>
