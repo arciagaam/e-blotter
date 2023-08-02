2995e5639cbfa53e0fad16aab7d38f7fd102a91d
polaski0 <meynard.trinidad44@gmail.com>
2023-08-02 14:43:43 +0800

    Minor fixes in record

--- app/Http/Controllers/admin/RecordController.php
+++ app/Http/Controllers/admin/RecordController.php
@@ -74,13 +74,6 @@ class RecordController extends Controller
      */
     public function print(string $record)
     {
-        return view('pages.admin.records.print', ['record' => Record::where('id', $record)->with('victim', 'suspect')->first()]);
+        return view('pages.admin.records.print', ['record' => Record::where('id', $record)->withTrashed()->with('victim', 'suspect')->first()]);
     }
 }
-
-// USER:
-// 1. sa blotter records po, pachange po nung bgry sa purok. kasi po for specific na brgy na sya, kaya po mas oks kung yung purok nalang
-
-// 2. patanggal po nung divorced, sa civil status
-
-// 3. diba po specific brgy po sila. nag try po ako mag gawa ng new record sa isang brgy. tapos po nag log po ulit ako ng isang brgy, start po yung blotter record sa 1 po ulit dapat. kasi po bukod silang blotter records
