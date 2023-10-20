09f3c77982bc6acd29f21bd75aa671cc785fb35f
polaski0 <meynard.trinidad44@gmail.com>
2023-10-20 14:15:41 +0800

    Edited vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'resources/js/vite';
+import laravel from 'resources/js/laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
@@ -8,18 +8,20 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
                 'resources/js/accounts.js',
+                'resources/js/admin_chart.js',
                 'resources/js/alert.js',
                 'resources/js/audio_record.js',
                 'resources/js/bootstrap.js',
                 'resources/js/calendar.js',
                 'resources/js/clear.js',
                 'resources/js/datetime.js',
-                'resources/js/nav.js',
-                'resources/js/table.js',
                 'resources/js/dynamic_input.js',
                 'resources/js/kp_form_select.js',
                 'resources/js/modal.js',
+                'resources/js/nav.js',
                 'resources/js/print_window.js',
+                'resources/js/table.js',
+                'resources/js/user_chart.js'
             ],
             refresh: true,
         }),
